4bb20c58891132f78c11b73705f18be5f875000d
Gabriel Santana <gabrielsapassos@gmail.com>
2026-01-30 20:03:40 -0300

    feat(user): enhance UserController with CRUD operations

--- src/main/java/com/gabrielsantana/todolist/user/UserController.java
+++ src/main/java/com/gabrielsantana/todolist/user/UserController.java
@@ -1,16 +1,44 @@
 package com.gabrielsantana.todolist.user;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("api/v1/users")
 public class UserController {
 
+    private final UserService service;
+
+    public UserController (UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = service.findAll();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<User> getUserByUsername(@PathVariable String username) {
+        User user = service.findByUsername(username);
+        return ResponseEntity.ok(user);
+    }
+
     @PostMapping("")
-    public void create(@RequestBody User user) {
-        System.out.println(user.getName());
+    public ResponseEntity<User> createUser(@RequestBody User userRequest) {
+        User user = service.create(userRequest);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(user);
     }
 }
