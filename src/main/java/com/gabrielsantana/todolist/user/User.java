c394ef7b3b204f79a93bf70254375fec13cf947a
Gabriel Santana <gabrielsapassos@gmail.com>
2026-01-30 19:55:13 -0300

    refactor(user): change username to be unique

--- src/main/java/com/gabrielsantana/todolist/user/User.java
+++ src/main/java/com/gabrielsantana/todolist/user/User.java
@@ -1,10 +1,6 @@
 package com.gabrielsantana.todolist.user;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,6 +19,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    @Column(unique = true)
     private String username;
     private String name;
     private String password;
