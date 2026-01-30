e1c9bbce309abfa861dbc1a9ec71640e42e34e9a
Gabriel Santana <gabrielsapassos@gmail.com>
2026-01-30 10:51:14 -0300

    refactor User Model from id type to UUID

--- src/main/java/com/gabrielsantana/todolist/user/User.java
+++ src/main/java/com/gabrielsantana/todolist/user/User.java
@@ -1,13 +1,32 @@
 package com.gabrielsantana.todolist.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
     private String username;
     private String name;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
