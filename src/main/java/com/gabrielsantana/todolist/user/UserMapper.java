7638271f4e3bfb86ae846a078388202fb3bc851a
Gabriel Santana <gabrielsapassos@gmail.com>
2026-02-04 19:38:26 -0300

    feat(user): implement mapper

--- src/main/java/com/gabrielsantana/todolist/user/UserMapper.java
+++ src/main/java/com/gabrielsantana/todolist/user/UserMapper.java
@@ -1,4 +1,14 @@
 package com.gabrielsantana.todolist.user;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(
+        componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+)
 public interface UserMapper {
+    User toUserEntity(UserRequestDTO userRequestDTO);
+
+    UserResponseDTO toUserResponseDTO(User user);
 }
