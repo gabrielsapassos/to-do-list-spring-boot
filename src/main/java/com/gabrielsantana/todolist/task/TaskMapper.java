bc6b0232197687632e1aadd69a7b3831ed949283
Gabriel Santana <gabrielsapassos@gmail.com>
2026-02-04 17:23:38 -0300

    feat(task): create mapper class

---
+++ src/main/java/com/gabrielsantana/todolist/task/TaskMapper.java
@@ -0,0 +1,19 @@
+package com.gabrielsantana.todolist.task;
+
+import com.gabrielsantana.todolist.user.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(
+        componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+)
+public interface TaskMapper {
+    @Mapping(target = "user", source = "user")
+    @Mapping(target = "id", ignore = true)
+    Task toEntity(TaskRequestDTO requestDTO, User user);
+
+    @Mapping(target = "userId", source = "task.user.id")
+    TaskResponseDTO toResponseDTO(Task task);
+}
