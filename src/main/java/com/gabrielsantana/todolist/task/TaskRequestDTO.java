da25e9fd43ec2a24596aa89f9dbc0c224567aa1f
Gabriel Santana <gabrielsapassos@gmail.com>
2026-02-04 17:22:55 -0300

    feat(task): create request and response DTO classes

---
+++ src/main/java/com/gabrielsantana/todolist/task/TaskRequestDTO.java
@@ -0,0 +1,27 @@
+package com.gabrielsantana.todolist.task;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record TaskRequestDTO(
+        @NotNull
+        UUID userId,
+
+        @NotBlank
+        @Size(max = 75)
+        String title,
+
+        String description,
+
+        LocalDateTime startAt,
+
+        LocalDateTime endAt,
+
+        @NotNull
+        TaskPriority priority
+) {
+}
---
+++ src/main/java/com/gabrielsantana/todolist/task/TaskResponseDTO.java
@@ -0,0 +1,19 @@
+package com.gabrielsantana.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record TaskResponseDTO(
+    Long id,
+    UUID userId,
+
+    String title,
+    String description,
+
+    LocalDateTime createdAt,
+    LocalDateTime startAt,
+    LocalDateTime endAt,
+
+    TaskPriority priority
+) {
+}
