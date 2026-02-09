e837742139818e62b6bbfcc2c22324af840f4674
Gabriel Santana <gabrielsapassos@gmail.com>
2026-02-09 15:35:03 -0300

    feat(task): implement search by user

--- src/main/java/com/gabrielsantana/todolist/task/TaskRepository.java
+++ src/main/java/com/gabrielsantana/todolist/task/TaskRepository.java
@@ -1,6 +1,11 @@
 package com.gabrielsantana.todolist.task;
 
+import com.gabrielsantana.todolist.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    Optional<List<Task>> findByUser(User user);
 }
