ec7a52363d6da2a2b23be92fbce696c54b34b873
Gabriel Santana <gabrielsapassos@gmail.com>
2026-02-13 09:12:19 -0300

    feat(user): add request validations in UserRequestDTO

--- src/main/java/com/gabrielsantana/todolist/user/UserRequestDTO.java
+++ src/main/java/com/gabrielsantana/todolist/user/UserRequestDTO.java
@@ -1,8 +1,17 @@
 package com.gabrielsantana.todolist.user;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UserRequestDTO(
+        @NotBlank(message = "Username cannot be blank")
         String username,
+
+        @NotBlank(message = "Name cannot be blank")
         String name,
+
+        @NotBlank(message = "Password cannot be blank")
+        @Size(min = 6, message = "Password must contain at least 6 digits")
         String password
 ) {
 }
