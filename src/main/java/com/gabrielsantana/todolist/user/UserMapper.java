146157339ef90c643d83dbb3c0eeb1a84756c984
Gabriel Santana <gabrielsapassos@gmail.com>
2026-02-13 09:11:46 -0300

    feat(user): add updateEntityFromDTO method and condition for non-blank string mapping in UserMapper

--- src/main/java/com/gabrielsantana/todolist/user/UserMapper.java
+++ src/main/java/com/gabrielsantana/todolist/user/UserMapper.java
@@ -1,6 +1,8 @@
 package com.gabrielsantana.todolist.user;
 
+import org.mapstruct.Condition;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import java.util.List;
@@ -15,4 +17,13 @@ public interface UserMapper {
 
     List<UserResponseDTO> toUserResponseDTOList (List<User> users);
     UserResponseDTO toUserResponseDTO(User user);
+
+    // @MappingTarget ensures that the Mapper won't create a new User entity, but instead updates the existing one
+    void updateEntityFromDTO(UserUpdateDTO userUpdateDTO, @MappingTarget User entity);
+
+    // Condition to ignore null or blank strings during mapping
+    @Condition
+    default boolean isNotBlank(String value) {
+        return value != null && !value.isBlank();
+    }
 }
