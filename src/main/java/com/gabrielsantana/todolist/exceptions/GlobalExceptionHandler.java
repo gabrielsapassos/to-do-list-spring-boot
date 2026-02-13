680a6765e61a4cc96226b1ecff75330fd9d96b8e
Gabriel Santana <gabrielsapassos@gmail.com>
2026-02-13 09:24:04 -0300

    feat(exception): implement specific message for each error in put user request

--- src/main/java/com/gabrielsantana/todolist/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/gabrielsantana/todolist/exceptions/GlobalExceptionHandler.java
@@ -4,10 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -37,5 +40,14 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleNotValid(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+
+        e.getBindingResult().getFieldErrors().forEach(ex -> errors.put(ex.getField(), ex.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
     }
 }
