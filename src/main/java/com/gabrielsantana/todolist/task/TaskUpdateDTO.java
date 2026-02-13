a8bbefda89aaba6c0e9eb785d398398ce6279284
Gabriel Santana <gabrielsapassos@gmail.com>
2026-02-13 10:11:38 -0300

    feat(task): add TaskUpdateDTO for update requests

---
+++ src/main/java/com/gabrielsantana/todolist/task/TaskUpdateDTO.java
@@ -0,0 +1,18 @@
+package com.gabrielsantana.todolist.task;
+
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+public record TaskUpdateDTO(
+        @Size(min = 3, message = "Title must have at least 3 digits")
+        String title,
+
+        String description,
+
+        LocalDateTime startAt,
+        LocalDateTime endAt,
+
+        TaskPriority priority
+) {
+}
